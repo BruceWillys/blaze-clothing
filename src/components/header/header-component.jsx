72f414f7aed2f695a2dc3c443e1dbe1eb58c50c7
Bruce Willys <brucewillys24@gmail.com>
2020-08-27 23:12:36 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/header/header-component.jsx
+++ src/components/header/header-component.jsx
@@ -35,7 +35,8 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 );
 
-const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+const mapStateToProps = ({user: { currentUser },
+    cart: { hidden }}) => ({
     currentUser,
     hidden
 });
