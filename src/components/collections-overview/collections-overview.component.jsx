fb10794f6e0eaa4ccd567debba32d737e799e4e9
Bruce Willys <brucewillys24@gmail.com>
2022-05-31 21:07:19 +0100

    updated program styles to use styled components instead of sass

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -6,16 +6,16 @@ import CollectionPreview from '../collection-preview/collection-preview.componen
 
 import {selectCollectionsForPreview} from '../../redux/shop/shop.selector';
 
-import './collections-overview.styles.scss';
+import { CollectionsOverviewContainer } from './collections-overview.styles.jsx';
 
 const CollectionsOverview = ({ collections }) => (
-    <div className='collection-overview'>
+    <CollectionsOverviewContainer>
         {
             collections.map(({ id, ...otherCollectionProps }) => (
                 <CollectionPreview key={id} {...otherCollectionProps} />
             ))
         }
-    </div>
+    </CollectionsOverviewContainer>
 )
 
 const mapStateToProps = createStructuredSelector({
